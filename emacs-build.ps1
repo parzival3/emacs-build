531460e1c48fc5905d6bc0c41e2b22ecec8c2cab
Enrico Tolotto (ENTO) <ento@eposaudio.com>
2022-01-12 08:50:50 +0100

    add ps1 for downloading emacs

---
+++ emacs-build.ps1
@@ -0,0 +1,4 @@
+﻿$emacs_url = "https://ftp.gnu.org/gnu/emacs/windows/emacs-27/emacs-27.2-x86_64-no-deps.zip"
+$emacs_zip = "emacs.zip"
+Invoke-WebRequest -Uri $emacs_url -OutFile $emacs_zip
+Expand-Archive -Path $emacs_zip 
